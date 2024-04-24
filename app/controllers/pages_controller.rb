352cc0d0e3a8f6d94b467a5cf99490fd35e649fc
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-24 21:46:14 +0000

    why not working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+    def main
+      render({ :template => "main"})
+    end
+    def display
+      render ({:template => "display"})
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+get ("/", { :controller => "pages", :action => "main"})
+get ("/dice", { :controller => "pages", :action => "display"})
 end
